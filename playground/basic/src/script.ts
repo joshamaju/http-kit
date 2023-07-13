d8d6b261f60a0f02c901bde26a5c27d5a812e379
Joshua Amaju <princejosh0007@gmail.com>
2023-07-14 00:32:32 +0100

    chore

--- playground/basic/src/script.ts
+++ playground/basic/src/script.ts
@@ -1,5 +1,7 @@
 import { request } from "http-kit";
 import * as Fetcher from "http-kit/fetch";
+import * as Fiber from "@effect/io/Fiber";
+import * as Either from "@effect/data/Either";
 import { searchParams } from "http-kit/function";
 import { filterStatusOk, parseJson, StatusError } from "http-kit/response";
 
@@ -24,6 +26,22 @@ class ReqRes {
       filterStatusOk(),
       parseJson<{ data: User }>(),
       Effect.map((res) => res.data),
+      // Effect.flatMap((data) => {
+      //   console.log("step data: ", data);
+      //   return pipe(
+      //     request("https://reqres.in/api/users/23"),
+      //     filterStatusOk(),
+      //     parseJson<{ data: User }>(),
+      //     Effect.flatMap((data) => {
+      //       console.log("step 2 data: ", data);
+      //       return pipe(
+      //         request("https://reqres.in/api/users/2"),
+      //         filterStatusOk(),
+      //         parseJson<{ data: User }>()
+      //       );
+      //     })
+      //   );
+      // }),
       // Effect.catchTag("StatusError", (e) => {
       //   return Effect.succeed("");
       // }),
@@ -39,45 +57,74 @@ class ReqRes {
   }
 }
 
-console.log(
+// const fiber = Effect.runFork(
+//   Fetcher.run(ReqRes.getUsers(2), [
+//     // {
+//     //   name: "custom-interceptor",
+//     //   request(req) {
+//     //     // return pipe(
+//     //     //   Effect.succeed(new Response(JSON.stringify({ data: { a: 1 } }))),
+//     //     //   Effect.delay(Duration.seconds(5))
+//     //     // );
+//     //     return Effect.delay(Effect.succeed(req), Duration.seconds(5));
+//     //   },
+//     // },
+//     // {
+//     //   request(req) {
+//     //     return Effect.gen(function* (_) {
+//     //       const res = yield* _(
+//     //         request("https://reqres.in/api/users/2"),
+//     //         parseJson()
+//     //       );
+//     //       console.log("res: ", res);
+//     //       const ares = yield* _(
+//     //         request("https://reqres.in/api/users/23"),
+//     //         filterStatusOk(),
+//     //         parseJson()
+//     //       );
+//     //       console.log("ares: ", ares);
+//     //       return new Request("https://reqres.in/api/users/2");
+//     //       // return req;
+//     //     });
+//     //   },
+//     // },
+//   ])
+// );
+
+// Effect.runFork(Effect.delay(Fiber.interrupt(fiber), Duration.seconds(5)));
+
+// setTimeout(() => {
+//   Effect.runFork(Fiber.interrupt(fiber));
+// }, 200);
+
+document.querySelector("button")?.addEventListener("click", () => {
   Effect.runFork(
     Fetcher.run(ReqRes.getUsers(2), [
       {
-        name: "custom-interceptor",
         request(req) {
-          return pipe(
-            Effect.succeed(new Response(JSON.stringify({ data: { a: 1 } }))),
-            Effect.delay(Duration.seconds(5))
-          );
-        },
-      },
-      {
-        request(req) {
-          return Effect.gen(function* (_) {
-            const res = yield* _(
-              request("https://reqres.in/api/users/2"),
-              parseJson()
+          return Effect.gen(function* (s) {
+            const a = yield* s(
+              pipe(
+                request("https://reqres.in/api/users/2"),
+                filterStatusOk(),
+                parseJson<{ a: number }>()
+                // Effect.either,
+                // Effect.map(
+                //   Either.match(
+                //     (err) => Effect.succeed("error"),
+                //     (data) => {
+                //       return Effect.succeed("data");
+                //     }
+                //   )
+                // )
+              )
             );
 
-            console.log("res: ", res);
-
-            const ares = yield* _(
-              request("https://reqres.in/api/users/23"),
-              filterStatusOk(),
-              parseJson()
-            );
-
-            console.log("ares: ", ares);
-
-            return new Request("https://reqres.in/api/users/2");
-            // return req;
+            console.log("a: ", a);
+            return req;
           });
         },
       },
     ])
-  )
-);
-
-// Effect(Effect.succeed(10))
-
-// console.log(Effect.runSync(Effect.succeed(10000)));
+  );
+});
