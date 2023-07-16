3babe5ac490dafbad12a0bf15898e2e8326d6879
Joshua Amaju <princejosh0007@gmail.com>
2023-07-16 18:41:03 +0100

    chore

--- packages/kit/src/index.ts
+++ packages/kit/src/index.ts
@@ -1,5 +1,7 @@
 export * from "./mod.js";
+export * from "./function.js";
 export * from "./exception.js";
-export type * from "./types.js";
 export * from "./execute/mod.js";
+
+export type * from "./types.js";
 export type * from "./execute/types.js";
