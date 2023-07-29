27097574794c63dc939f35b51b32dacbd777581a
Joshua Amaju <princejosh0007@gmail.com>
2023-07-29 10:44:36 +0100

    refractor

--- packages/kit/src/types.ts
+++ packages/kit/src/types.ts
@@ -1,9 +1,9 @@
 import type * as Effect from "@effect/io/Effect";
 import { Interpreter } from "./interpreter.js";
 import { Err } from "./exception.js";
-import { Request } from "./request/Request.js";
+import { HttpRequest } from "./request/Request.js";
 
-export type Req = Request;
+export type Req = HttpRequest;
 
 export type Res = Response;
 
