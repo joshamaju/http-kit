6bc4a3e5d6a4ff5ca97b283e93d35df6dcdc4a7a
Joshua Amaju <princejosh0007@gmail.com>
2023-07-29 11:22:49 +0100

    chore

--- playground/basic/src/script.ts
+++ playground/basic/src/script.ts
@@ -1,12 +1,18 @@
-import * as Fetcher from "http-kit/fetch";
+import * as Fetch from "http-kit/fetch";
+import * as Http from "http-kit";
 import { searchParams } from "http-kit/function";
 import * as Req from "http-kit/request";
-import { filterStatusOk, parseJson } from "http-kit/response";
+import { filterStatusOk, toJson } from "http-kit/response";
 
 import { pipe } from "@effect/data/Function";
+import * as Context from "@effect/data/Context";
+import * as Option from "@effect/data/Option";
 import * as Effect from "@effect/io/Effect";
 import * as Logger from "@effect/io/Logger";
 import * as LogLevel from "@effect/io/Logger/Level";
+
+import * as Log from "effect-log";
+import { HttpRequest } from "http-kit/request";
 
 interface User {
   id: number;
@@ -19,14 +25,18 @@ interface User {
 class ReqRes {
   static getUsers(page?: number) {
     return pipe(
-      Req.get("/api/users/", {
+      Req.get("/api/users/?age=10", {
         search: searchParams({ page }),
       }),
       filterStatusOk(),
-      parseJson<{ data: User }>(),
+      toJson<{ data: User[] }>(),
       Effect.map((res) => res.data),
       Effect.tap((data) => Effect.log(JSON.stringify(data))),
-      Effect.catchAllCause(Effect.logErrorCause)
+      Effect.flatMap((user) => Req.get(`/api/users/${user[0].id}`)),
+      filterStatusOk(),
+      toJson<{ data: User }>(),
+      Effect.tap((data) => Effect.log(JSON.stringify(data)))
+      // Effect.catchAllCause(Effect.logErrorCause)
     );
   }
 }
@@ -35,30 +45,97 @@ export function isAbsolute(u: string): boolean {
   return u.indexOf("http://") === 0 || u.indexOf("https://") === 0;
 }
 
+interface Store {
+  get(): Option.Option<string>;
+}
+
+const Store = Context.Tag<Store>();
+
+const StoreLive = Store.of({ get: () => Option.some("") });
+
+// function get() {
+//   return pipe(
+//     Req.get("/api/users/?age=10"),
+//     filterStatusOk(),
+//     toJson<{ data: User }>(),
+//     Effect.flatMap((d) => {
+//       return Effect.flatMap(Store, (store) => Effect.sync(() => store.get()));
+//     })
+//   );
+// }
+
+let n = pipe(
+  Req.get("/api/users/?age=10"),
+  filterStatusOk(),
+  toJson<{ data: User }>(),
+  Effect.flatMap((d) => {
+    return Effect.flatMap(Store, (store) => Effect.sync(() => store.get()));
+  })
+);
+
+let m = pipe(
+  n,
+  // Effect.provideService(Store, StoreLive),
+  Http.provide(Fetch.adapter, { request: (req) => Effect.succeed(req) })
+  // Effect.runPromise
+);
+
+// const res = pipe(
+//   Fetcher.withInterceptors([])(get()),
+//   Effect.provideService(Store, StoreLive),
+//   Logger.withMinimumLogLevel(LogLevel.Debug),
+//   Effect.runPromise
+// );
+
+// const res = pipe(
+//   get(),
+//   Fetch.provide,
+//   Effect.provideService(Store, StoreLive),
+//   Logger.withMinimumLogLevel(LogLevel.Debug),
+//   Effect.runPromise
+// );
+
 Effect.runFork(
   pipe(
-    Fetcher.provide(ReqRes.getUsers(2), [
+    ReqRes.getUsers(2),
+    Http.provide(
+      Fetch.adapter,
       {
         request(req) {
-          const clone = req.clone();
+          return Effect.gen(function* (_) {
+            const clone = req.clone();
 
-          const headers = new Headers(clone.init.headers);
+            const headers = new Headers(clone.init.headers);
 
-          headers.set("Authorization", "Bearer token");
+            headers.set("Authorization", "Bearer token");
 
-          const url = isAbsolute(clone.url)
-            ? clone.url
-            : new URL(clone.url, "https://reqres.in");
+            const url = isAbsolute(clone.url)
+              ? clone.url
+              : new URL(clone.url, "https://reqres.in");
 
-          return Effect.succeed(
-            new Req.Request(url, { ...clone.init, headers })
-          );
+            const res = yield* _(Req.request(url, clone.init));
+
+            console.log("res: ", res);
+
+            return res;
+          });
+
+          // return Req.request(url, clone.init);
         },
         response(res) {
           return Effect.succeed(res);
         },
       },
-    ]),
+      {
+        request(req) {
+          console.warn("intercept");
+          return Effect.succeed(req);
+        },
+      }
+    ),
+    // Effect.provideLayer(
+    //   Logger.replace(Logger.defaultLogger, Log.pretty as any)
+    // ),
     Logger.withMinimumLogLevel(LogLevel.Debug)
   )
 );
