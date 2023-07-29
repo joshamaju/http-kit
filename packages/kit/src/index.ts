10f54b74b11bdaf1e882b989e2dc154504b3fe77
Joshua Amaju <princejosh0007@gmail.com>
2023-07-29 09:03:04 +0100

    refactor

--- packages/kit/src/index.ts
+++ packages/kit/src/index.ts
@@ -1,7 +1,7 @@
 export * from "./function.js";
 export * from "./exception.js";
-export * from "./execute/mod.js";
+export * from "./executor/mod.js";
 export * from "./request/mod.js";
 
 export type * from "./types.js";
-export type * from "./execute/types.js";
+export type * from "./executor/types.js";
